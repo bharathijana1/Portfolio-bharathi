7cb3ac9c483b5286a17b37d6eeab1c72c0945225
bharathijana1 <160519295+bharathijana1@users.noreply.github.com>
2025-02-11 17:21:10 +0530

    Update NavBar.jsx

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -112,7 +112,8 @@ const NavBar = () => {
         background: 'rgb(52,124,152)',
         background: 'radial-gradient(circle, rgba(127,51,171,1) 0%, rgba(80,23,112,1) 69%, rgba(29,0,45,1) 100%)',
       }}>
-      <a href="./src/Components/img/Bharathi_ResumeC.pdf" download=true target='_blank'>Download CV</a>
+{/*       <a href="./src/Components/img/Bharathi_ResumeC.pdf" download=true target='_blank'>Download CV</a> */}
+      <a href="/Bharathi_ResumeC.pdf" download target='_blank'>Download CV</a>
     </button>
   </div>
 )}
