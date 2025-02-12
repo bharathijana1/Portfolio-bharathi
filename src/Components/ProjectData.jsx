0ebb056a5f28a49d0c998ff6225b49ed895a06b7
bharathijana1 <160519295+bharathijana1@users.noreply.github.com>
2025-02-12 13:00:52 +0530

    Update ProjectData.jsx

--- src/Components/ProjectData.jsx
+++ src/Components/ProjectData.jsx
@@ -27,168 +27,168 @@ export default [
     {
         img: FormvalidationImg,
         title: "Form Validation",
-        button: "Go To Netlify",
+        button: "View Demo",
         alt: "Form validation",
         ahref: "https://formvalide.netlify.app/"
     },
     {
         img: LearnandgrowImg,
         title: "Learn And Grow",
-        button: "Go To Netlify",
+        button: "View Demo",
         alt: "Learn And Grow",
         ahref: "https://learn-and-grow.netlify.app/"
     },
     {
         img: LibraryManagementsystemImg,
         title: "Library Managment system",
-        button: "Go To Netlify",
+        button: "View Demo",
         alt: "Library Managment system",
         ahref: "https://librarymanagementsys.netlify.app/"
     },
     {
         img: FreshcartImg,
         title: "FreshCart E-Commerce",
-        button: "Go To Netlify",
+        button: "View Demo",
         alt: "FreshCart",
         ahref: "https://e-commerce-freshcart-website.netlify.app"
     },
     {
         img: AdviserappImg,
         title: "Adviser APP",
-        button: "Go To Netlify",
+        button: "View Demo",
         alt: "Adviser APP",
         ahref: "https://adviser-application.netlify.app"
     },
     {
         img: GenerateqrcodeImg,
         title: "Generate QR Code",
-        button: "Go To Netlify",
+        button: "View Demo",
         alt: "Generate QR Code",
         ahref: "https://generate-qr-code-for-website.netlify.app"
     },
     {
         img: WeatherappImg,
         title: "Weather API App",
-        button: "Go To Netlify",
+        button: "View Demo",
         alt: "Weather API App",
         ahref: "https://weather-api-app-proj.netlify.app"
     },
     {
         img: BmicalculatorImg,
         title: "BMI Calculator",
-        button: "Go To Netlify",
+        button: "View Demo",
         alt: "BMI calulator",
         ahref: "https://bim-calculator-app.netlify.app/"
     },
     {
         img: FoodcourtImg,
         title: "Foodcourt",
-        button: "Go To Netlify",
+        button: "View Demo",
         alt: "Foodcourtr",
         ahref: "https://food-court-web.netlify.app/"
     },
     {
         img: CurrencyconvertorImg,
         title: "Currency Converter",
-        button: "Go To Github",
+        button: "View Code",
         alt: "Currency Converter",
         ahref: "https://github.com/bharathijana1/Currency-Converter-REACT"
     },
     {
         img: ProfilecardImg,
         title: "Profile Card",
-        button: "Go To Github",
+        button: "View Code",
         alt: "Profile Card",
         ahref: "https://github.com/bharathijana1/profilecard-props-REACT"
     },
     {
         img: QuizgameImg,
         title: "Quizgame",
-        button: "Go To Github",
+        button: "View Code",
         alt: "Quizgame",
         ahref: "https://github.com/bharathijana1/QuizGame-REACTJS"
     },
     {
         img: StopwatchImg,
         title: "Stop watch",
-        button: "Go To Github",
+        button: "View Code",
         alt: "Stop watch",
         ahref: "https://github.com/bharathijana1/StopWatch-REACTJS"
     },
     {
         img: WeatherAppImg,
         title: "Weather app",
-        button: "Go To Github",
+        button: "View Code",
         alt: "Weather app",
         ahref: "https://github.com/bharathijana1/WeatherApp-REACTJS"
     },
     {
         img: StudioImg,
         title: "Studio",
-        button: "Go To Github",
+        button: "View Code",
         alt: "Studio",
         ahref: "https://github.com/bharathijana1/studio_PROJ-HTML"
     },
     {
         img: ToursandtravelsImg,
         title: "Tours and travels",
-        button: "Go To Github",
+        button: "View Code",
         alt: "Tours and travels",
         ahref: "https://github.com/bharathijana1/Tour-and-travells"
     },
     {
         img: CalculationImg,
         title: "Calculation",
-        button: "Go To Github",
+        button: "View Code",
         alt: "Calculation",
         ahref: "https://github.com/bharathijana1/Calculation-REACTJS"
     },
     {
         img: DigitalclockImg,
         title: "Digital clock",
-        button: "Go To Github",
+        button: "View Demo",
         alt: "Digital clock",
         ahref: "https://digitclk.netlify.app/"
     },
     {
         img: PhotozoneImg,
         title: "Photozone",
-        button: "Go To Github",
+        button: "View Code",
         alt: "Photozone",
         ahref: "https://github.com/bharathijana1/photozone-res_PROJ-HTML-CSS-JS"
     },
     {
       img: QuizImg,
       title: "Quiz Game",
-      button: "Go To Github",
+      button: "View Demo",
       alt: "Quiz Game",
-      ahref: "https://github.com/bharathijana1/Quiz-App-Withtimeleft-REACTJS"
+      ahref: "https://quiz-game-app-with-timeleft.netlify.app/"
     },
     {
       img: PasswordgeneratorImg,
       title: "Password Generator",
-      button: "Go To Github",
+      button: "View Code",
       alt: "Password Generator",
       ahref: "https://github.com/bharathijana1/Password-Generator-REACTJS"
     },
     {
       img: CalendorImg,
       title: "Calender",
-      button: "Go To Github",
+      button: "View Code",
       alt: "Calender",
       ahref: "https://github.com/bharathijana1/Calender-REACTJS"
     },
     {
       img: ClockImg,
       title: "Digital Clock",
-      button: "Go To Github",
+      button: "View Code",
       alt: "Digital Clock",
       ahref: "https://github.com/bharathijana1/Digital-Clock-Tailwind-REACTJS"
     },
     {
       img: AccordionImg,
       title: "Accordion",
-      button: "Go To Github",
+      button: "View Code",
       alt: "Accordion",
       ahref: "https://github.com/bharathijana1/FAQ-Accordion-REACTJS"
     },
