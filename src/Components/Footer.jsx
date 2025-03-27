6f159e082e8a0f7d1d5a6bc2a24c644a89cbf110
bharathijana1 <160519295+bharathijana1@users.noreply.github.com>
2025-03-27 10:58:12 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='bg-black text-white text-center p-3 text-xl  mt-10 py-10'>
-        Designed and Developed by bharathi copyrights&copy;2024
+        Designed and Developed by bharathi copyrights&copy;2025
       
     </div>
   )
