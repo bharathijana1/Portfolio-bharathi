47986dad09c53ab9d787096ba332f017ce768a76
bharathijana1 <160519295+bharathijana1@users.noreply.github.com>
2025-03-27 10:59:23 +0530

    Update PortfolioHome.jsx

--- src/Components/PortfolioHome.jsx
+++ src/Components/PortfolioHome.jsx
@@ -8,6 +8,7 @@ import 'aos/dist/aos.css';
 import Academic from './Academic';
 import Skills from './Skills';
 import HeroImg from '../Components/img/hero.jpg';
+import { Contact_Details } from './Contact_Details';
 
 const PortfolioHome = () => {
   useEffect(() => {
@@ -18,15 +19,16 @@ const PortfolioHome = () => {
     });
   }, []);
   return (
-    <div>
-        <div className='flex flex-col-reverse md:flex-row lg:flex-row justify-around items-center mx-auto lg:mx-5 lg:py-5 h-screen'>
+    <div className=''>
+        <div className=' flex flex-col-reverse md:flex-row lg:flex-row justify-around items-center mx-auto lg:mx-5 lg:py-5 lg:h-screen'>
             <div className=' p-1 md:p-3 m-1 flex justify-start flex-col w-full xl:w-1/2' data-aos="fade-down">
             <h1 className='text-3xl md:text-4xl text-center md:text-start p-1'>Hello</h1>
             <h1 className="text-center md:text-start p-1 font-bold text-3xl md:text-4xl">I'm <span className='text-fuchsia-900 font-bold'>Bharathi</span></h1>
             <h1 className='text-3xl md:text-4xl font-bold text-center md:text-start p-1'>Front End Developer</h1>
             <h1 className='text-xl text-center md:text-start p-1'>Expert in responsive, dynamic & interactive website using HTML5, CSS, BOOTSTRAP, TAILWIND, JAVASCRIPT, REACT JS. I am a skilled and passionate web designer with experience in creating visually appealing and user-friendly websites.</h1>
-            <div className='flex justify-center mt-5'>
-            <button className='border bg-fuchsia-900 p-2 px-8 py-3 m-2 text-lg font-medium text-white' 
+            <div className='flex justify-center item-center flex-col lg:flex-row mt-5'>
+            <div className='flex justify-center'>
+            <button className='border bg-fuchsia-900 p-2 px-8 py-3 m-2 text-lg font-medium text-white ' 
             style={{
             background: 'rgb(52,124,152)',
             background: 'radial-gradient(circle, rgba(127,51,171,1) 0%, rgba(80,23,112,1) 69%, rgba(29,0,45,1) 100%)',
@@ -34,6 +36,8 @@ const PortfolioHome = () => {
             
         <a href="./Bharathij_Resume.pdf" download target='_blank' className='uppercase' >Download CV</a>
        </button>
+       </div>
+       <div className='flex justify-center'>
        <Link to='projects'>
        <button className='border bg-fuchsia-900 p-2 px-8 py-3 m-2 text-lg font-medium text-white' 
             style={{
@@ -43,11 +47,12 @@ const PortfolioHome = () => {
             <h1 className='uppercase'>Projects</h1> 
        </button>
        </Link>
+       </div>
        
             </div>
             
             </div>
-            <div className='p-3 md:p-2'>
+            <div className='p-3 md:p-2 w-full xl:w-1/2 '>
                 <img src={HeroImg} alt="home" className='hover:shadow-md hover:shadow-fuchsia-800' />
             </div>
             
@@ -61,6 +66,12 @@ const PortfolioHome = () => {
         <Skills />
         <ProjectsMain />
         <Contact />
+        <Contact_Details />
+
+        
+
+
+        
         
       
     </div>
